1dee079ee67360168d2cce17e9c7f0001facd23d
R-Huerlimann <R-Huerlimann@users.noreply.github.com>
2024-04-04 16:20:28 +0900

    Update 01_malabar_softmask_genome.sh

--- 05_braker_genome_annotation/01_malabar_softmask_genome.sh
+++ 05_braker_genome_annotation/01_malabar_softmask_genome.sh
@@ -4,7 +4,7 @@ ml bedtools/2.30.0
 
 cd /malabar/09_Final_resources/
 
-repeatsP1=/malabar/03_repeat_annotation/merged_repeats_unique.gff3
-genomeP1=Emal_V1_24Chr_P0_rn.fasta
+repeatsP0=/malabar/03_repeat_annotation/merged_repeats_unique.gff3
+genomeP0=Emal_V1_24Chr_P0_rn.fasta
 
-bedtools maskfasta -soft -fi ${genomeP1} -bed ${repeatsP1} -fo ${genomeP1/_rn/_rn_sm}
+bedtools maskfasta -soft -fi ${genomeP0} -bed ${repeatsP0} -fo ${genomeP0/_rn/_rn_sm}

aad937d399d68eeaf9115f7ba6e466e468ded2da
R-Huerlimann <R-Huerlimann@users.noreply.github.com>
2024-04-05 11:41:27 +0900

    Update and rename 01_splitfasta_iprscan.sh to 01_splitfasta.sh

--- 06_gene_models_functional_annotation/01_splitfasta.sh
+++ 06_gene_models_functional_annotation/01_splitfasta.sh
@@ -9,10 +9,10 @@ split_fasta(){
 
 ### Directories and Files ###
 DIRin=/malabar/05_braker_annotation/emal_braker_RNA_PROT_all
-FILEaa=${inDir}/augustus.hints_anysupport_nosupportwithdomaindiamond_longest_isoform.aa
+FILEaa=augustus.hints_anysupport_nosupportwithdomaindiamond_longest_isoform.aa
 name=malabar
 DIRout=/malabar/06_gene_models_functional_annotation
 
 ### code ###
 cd ${DIRout}
-split_fasta ${FILEaa} $name
+split_fasta ${inDir}/${FILEaa} ${name}

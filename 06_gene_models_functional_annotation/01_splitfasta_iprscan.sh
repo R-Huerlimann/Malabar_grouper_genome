e8a0ae359b98a768ec1c2de59a30992945e8bd75
R-Huerlimann <R-Huerlimann@users.noreply.github.com>
2024-04-05 11:28:54 +0900

    Update 01_splitfasta_iprscan.sh

--- 06_gene_models_functional_annotation/01_splitfasta_iprscan.sh
+++ 06_gene_models_functional_annotation/01_splitfasta_iprscan.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+### Functions ###
+split_fasta(){
+	input=$1
+	species_in=$2
+	cat $input | bioawk -c 'fastx' -v species_in=$2 '{ if( (NR-1)%1000==0 ){file=sprintf("%s%d.fa",species_in,(NR-1));} printf(">%s\t%s\n%s\n",$name,$comment,$seq) >> file}'
+}
+
+### Directories and Files ###
+DIRin=/malabar/05_braker_annotation/emal_braker_RNA_PROT_all
+FILEaa=${inDir}/augustus.hints_anysupport_nosupportwithdomaindiamond_longest_isoform.aa
+name=malabar
+DIRout=/malabar/06_gene_models_functional_annotation
+
+### code ###
+cd ${DIRout}
+split_fasta ${FILEaa} $name
